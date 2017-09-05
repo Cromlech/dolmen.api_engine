8bca60e42de11c50017810930fb788a820bf59dd
Souheil CHELFOUH <trollfot@gmail.com>
2017-09-06 00:09:55 +0200

    Added authentication skeleton

---
+++ src/dolmen/api_engine/auth.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+class authenticate(object):
+
+    def __init__(self, checkers, **conf):
+        self.checkers = checkers
+        self.conf = conf
+
+    def __call__(self, app):
+        def method_watchdog(environ, start_response):
+            auth = environ.get('HTTP_AUTHORIZATION')
+            if auth is not None:
+                authtype, authvalue = auth.split(' ', 1)
+                checker = self.checkers.get(authtype)
+                if checker is not None:
+                    code, payload = self.checker(
+                        authvalue, environ, self.conf)
+
+                    if code == 200:
+                        environ['auth_payload'] = payload
+                        return app(environ, start_response)
+                    else:
+                        return reply(code, text=payload)(
+                            environ, start_response)
+                except Exception as e:
+                    # log me
+                    raise
+            return reply(401)(environ, start_response)
+        return method_watchdog
