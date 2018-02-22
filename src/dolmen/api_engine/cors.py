db42bee776df99afba5310434e14a0a04798cc68
Souheil CHELFOUH <trollfot@gmail.com>
2018-02-22 01:50:29 +0100

    by default, allow_origins no longer discriminate on code 200. Corrected discrimination if/else as it was buggy

--- src/dolmen/api_engine/cors.py
+++ src/dolmen/api_engine/cors.py
@@ -3,12 +3,12 @@
 from functools import wraps
 
 
-def allow_origins(origins, codes=(200,)):
+def allow_origins(origins, codes=None):
     def cors_wrapper(method):
         @wraps(method)
         def add_cors_header(*args, **kwargs):
             res = method(*args, **kwargs)
-            if not codes or not res.status_int in codes:
+            if codes and not res.status_int in codes:
                 return res
             res.headers["Access-Control-Allow-Origin"] = origins
             return res
