ff8ec951c499661a0aaf2e9b1c51d76169d7c775
Souheil CHELFOUH <trollfot@gmail.com>
2018-02-22 01:30:36 +0100

    allow_origins now can discriminate on the response status code.

--- src/dolmen/api_engine/cors.py
+++ src/dolmen/api_engine/cors.py
@@ -3,11 +3,13 @@
 from functools import wraps
 
 
-def allow_origins(origins):
+def allow_origins(origins, codes=(200,)):
     def cors_wrapper(method):
         @wraps(method)
         def add_cors_header(*args, **kwargs):
             res = method(*args, **kwargs)
+            if not codes or not res.status_int in codes:
+                return res
             res.headers["Access-Control-Allow-Origin"] = origins
             return res
         return add_cors_header
